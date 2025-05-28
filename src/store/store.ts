bf48a6d53a90d9b53508addc82050083ec019248
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-27 23:05:15 -0500

    🚀 feat: integrar llamada a API con Axios
    - Se añadió la librería `axios` como dependencia.
    - Se implementó la función `getCryptos` para realizar una petición a la API de criptomonedas.
    - Se actualizó el método `fetchCryptos` en el store `useCriptoStore` para utilizar `getCryptos`.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,15 @@
 import { create } from 'zustand'
+import axios from 'axios'
+
+const getCryptos = async () => {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+  const {data: {Data}} = await axios.get(url)
+  console.log(Data)
+
+}
 
 export const useCriptoStore = create(() => ({
-  fetchCryptos: () => {
-    console.log('Desde fetch cryptos')
+  fetchCryptos: async () => {
+    await getCryptos()
   }
 }))
