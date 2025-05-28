758bf3b8c7c571f6b852522210a3d5c2e1ec83b4
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-28 00:32:40 -0500

    🎉 feat: mostrar detalles adicionales de criptomonedas
    - Se agregó soporte para mostrar atributos adicionales como precio más alto, precio más bajo, variación en 24 horas y última actualización.
    - Se actualizó el estilo en `index.css` para soportar nuevas secciones de resultados.
    - Se incluyó la imagen de la criptomoneda en los detalles.

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -7,13 +7,18 @@ const CryptoPriceDisplay = () => {
   const hasResult = useMemo(() => Object.keys(result).length > 0, [result])
 
   return (
-    <div>
+    <div className={'result-wrapper'}>
       {hasResult && (
         <>
           <h2>Cotización</h2>
-          <div className="result">
+          <div className={'result'}>
+            <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="Imagen Cryptomoneda"/>
             <div>
-              <p>el precio es de: <span>{result.PRICE}</span></p>
+              <p>El precio es de: <span>{result.PRICE}</span></p>
+              <p>El precio más alto del día: <span>{result.HIGHDAY}</span></p>
+              <p>el precio más bajo del día: <span>{result.LOWDAY}</span></p>
+              <p>Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+              <p>Ultima actualización: <span>{result.LASTUPDATE}</span></p>
             </div>
           </div>
         </>
