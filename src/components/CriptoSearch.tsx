e6cca7649ca2f8facbbc01c191276b2c78a48fee
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-27 23:43:15 -0500

    🎉 feat: vincular selección de moneda y criptomoneda al estado
    - Se agregó un hook `useState` en `CriptoSearch` para gestionar el estado del par moneda/criptomoneda.
    - Se implementaron controladores `onChange` en los `select` para actualizar el estado.
    - Se creó el esquema `PairSchema` y su tipo asociado `Pair` para validar y manejar pares.
    - Se actualizó el archivo `schema/cripto-schema.ts` con el nuevo esquema.

--- src/components/CriptoSearch.tsx
+++ src/components/CriptoSearch.tsx
@@ -1,15 +1,23 @@
 import {currencies} from "../data";
 import {useCryptoStore} from "../store/store.ts";
+import {useState} from "react";
+import type {Pair} from "../types";
 
 const CriptoSearch = () => {
 
   const cryptoCurrencies = useCryptoStore(state => state.cryptoCurrencies)
+  const [pair, setPair] = useState<Pair>({
+    currency: '',
+    cripto_currency: ''
+  });
 
   return (
     <form className={'form'}>
       <div className={'field'}>
         <label htmlFor={'currency'}>Moneda:</label>
-        <select name={'currency'} id={'currency'}>
+        <select name={'currency'}
+                id={'currency'}
+                onChange={(e) => setPair({...pair, currency: e.target.value})}>
           <option value="">-- Seleccione --</option>
           {currencies.map((currency) => (
             <option key={currency.code} value={currency.code}>{currency.name}</option>
@@ -18,7 +26,9 @@ const CriptoSearch = () => {
       </div>
       <div className={'field'}>
         <label htmlFor={'cripto_currency'}>Criptomoneda:</label>
-        <select name={'cripto_currency'} id={'cripto_currency'}>
+        <select name={'cripto_currency'}
+                id={'cripto_currency'}
+                onChange={(e) => setPair({...pair, cripto_currency: e.target.value})}>
           <option value="">-- Seleccione --</option>
           {cryptoCurrencies.map((currency) => (
             <option key={currency.CoinInfo.Name} value={currency.CoinInfo.Name}>{currency.CoinInfo.FullName}</option>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,8 @@
 import {z} from 'zod/v4'
-import {CryptoCurrencyResponseSchema, CurrencySchema} from '../schema/cripto-schema'
+import {CryptoCurrencyResponseSchema, CurrencySchema, PairSchema} from '../schema/cripto-schema'
 
 export type Currency = z.infer<typeof CurrencySchema>
 
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
+
+export type Pair = z.infer<typeof PairSchema>
