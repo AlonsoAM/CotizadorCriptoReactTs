829f54aec6ac288798a0164de0f5170bfe547305
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-27 23:25:24 -0500

    🎉 feat: agregar soporte para criptomonedas en el store
    - Se añadió el esquema `CryptoCurrencyResponseSchema` y su tipo asociado `CryptoCurrency`.
    - Se extendió el store `useCriptoStore` para gestionar un arreglo de criptomonedas.
    - Se integró validación con Zod en la función `getCryptos` para asegurar la estructura de los datos.
    - Se ajustó la estructura del estado utilizando el middleware `devtools` de Zustand.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,29 @@
-import { create } from 'zustand'
+import {create} from 'zustand'
+import {devtools} from 'zustand/middleware'
 import axios from 'axios'
+import {CryptoCurrenciesResponseSchema} from "../schema/cripto-schema.ts";
+import type {CryptoCurrency} from "../types";
+
+type CryptoStore = {
+  cryptoCurrencies: CryptoCurrency[],
+  fetchCryptos: () => Promise<void>
+}
 
 const getCryptos = async () => {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   const {data: {Data}} = await axios.get(url)
-  console.log(Data)
-
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+  if (result.success) {
+    return result.data
+  }
 }
 
-export const useCriptoStore = create(() => ({
+export const useCriptoStore = create<CryptoStore>(devtools((set) => ({
+  cryptoCurrencies: [],
   fetchCryptos: async () => {
-    await getCryptos()
+    const cryptoCurrencies = await getCryptos()
+    set(() => ({
+      cryptoCurrencies
+    }))
   }
-}))
+})))
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
 import {z} from 'zod/v4'
-import {CurrencySchema} from '../schema/cripto-schema'
+import {CryptoCurrencyResponseSchema, CurrencySchema} from '../schema/cripto-schema'
 
 export type Currency = z.infer<typeof CurrencySchema>
+
+export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
